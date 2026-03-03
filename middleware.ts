2a4fecd7019b1e0afa58b3610dab9f711857d524
kishore600 <kishore.knaresh07@gmail.com>
2026-03-03 22:48:46 +0530

    updated middle ware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { verifyToken } from './lib/auth'
 
-export function proxy(request: NextRequest) { 
+export function middleware(request: NextRequest) { 
   const token = request.cookies.get('token')?.value
   const { pathname } = request.nextUrl
 
